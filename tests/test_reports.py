30548bd8fcc172cbace3d3e5c52db5f103198515
Gelena Bezusco <helenatcitok@gmail.com>
2023-12-04 20:07:43 +0000

    Initial

---
+++ tests/test_reports.py
@@ -0,0 +1,27 @@
+import pytest
+import pandas as pd
+from src.reports import spending_by_category
+import logging
+
+
+@pytest.fixture
+def sample_data():
+    return pd.DataFrame({
+        'Дата операции': ['2023-09-01 12:34:45', '2023-09-02 12:34:45', '2023-09-05 12:34:45', '2023-10-01 12:34:45', '2023-11-01 12:34:45'],
+        'Категория': ['Food', 'Clothing', 'Food', 'Clothing', 'Food'],
+        'Статус': ["OK", "OK", "OK", "OK", "OK"]
+    })
+
+
+def test_spending_by_category_default_filename(tmp_path, caplog, sample_data):
+    caplog.set_level(logging.INFO)
+    result = spending_by_category(sample_data, 'Food', date='2023-11-01 12:34:45')
+    assert "Операции выполнена" in caplog.text
+    assert "Файл успешно сохранен" in caplog.text
+
+
+def test_spending_by_category_custom_filename(tmp_path, caplog, sample_data):
+    caplog.set_level(logging.INFO)
+    result = spending_by_category(sample_data, 'Food', date='2023-11-01 12:34:45')
+    assert "Операции выполнена" in caplog.text
+    assert "Файл успешно сохранен" in caplog.text
